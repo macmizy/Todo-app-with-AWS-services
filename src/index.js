7a520276316c0b442923cad0b237697e505db8d4
Macmizy <macmizy@gmail.com>
2023-09-27 23:06:52 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Amplify from "aws-amplify";
+import awsConfig from "./aws-exports";
+
+Amplify.configure(awsConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
